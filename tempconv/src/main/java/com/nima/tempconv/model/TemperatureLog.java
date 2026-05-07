5784db46147f3ae96163e8ce21f871c6ee153e42
vikumkodikara <vikumkodikara123@gmail.com>
2026-05-08 00:09:33 +0530

    feat(tempconv): add TemperatureLog model entity
    Define TemperatureLog document class with MongoDB annotations for storing temperature conversion records including input value, converted value, and conversion type.

---
+++ tempconv/src/main/java/com/nima/tempconv/model/TemperatureLog.java
@@ -0,0 +1,25 @@
+package com.nima.tempconv.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Document(collection = "conversions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TemperatureLog {
+
+    @Id
+    private String id;
+    private double inputTemperature;
+    private String inputUnit;
+    private double outputTemperature;
+    private String outputUnit;
+    private String timestamp;
+}
